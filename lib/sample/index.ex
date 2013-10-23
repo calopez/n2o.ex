80dca1ef2403c15af711f8df3a3b4854d5fb0307
Maxim Sokhatsky <maxim@synrc.com>
2013-10-23 16:20:30 +0300

    polymorphic records

--- lib/sample/index.ex
+++ lib/sample/index.ex
@@ -1,6 +1,6 @@
 defmodule Sample.Index do
 
-    def main() do :dtl.new( file: "index", bindings: [title: "Elixir N2O", body: "N2O Sample"]) end
+    def main() do :dtl.new(file: "index", bindings: [title: "Elixir N2O", body: "N2O Sample"]) end
     def body() do "N2O Sample" end
     def event(e) do IO.puts "Event: #{inspect e}" end
 
--- lib/sample/records.ex
+++ lib/sample/records.ex
@@ -1,14 +1,14 @@
 defmodule N2O do
+    defmacro  element(modu) do quote do [ancestor: :element, module: unquote(modu), id: :undefined,
+              actions: [], class: [], style: [], source: [],
+              data_fields: [], aria_states: [], body: [], role: [],
+              tabindex: 0, show_if: false, html_tag: :undefined, title: []] end end
+end
 
-    defrecord :dtl, ancestor: :element, module: :element_dtl, id: :undefined,
-                    actions: [], class: [], style: [], source: [],
-                    data_fields: [],aria_states: [], body: [], role: [],
-                    tabindex: 0, show_if: false, html_tag: :undefined,
-                    title: "", file: "index", bindings: [], app: __DIR__,
-                    folder: "priv/templates", ext: "html", bind_script: true
-
+defmodule N2O.Records do
+    require N2O
     defrecord :context, Record.extract(:context, from_lib: "n2o/include/wf.hrl")
     defrecord :button, Record.extract(:button, from_lib: "n2o/include/wf.hrl")
     defrecord :http_req, Record.extract(:http_req, from_lib: "cowboy/src/cowboy_req.erl")
-
+    defrecord :dtl, N2O.element(:element_dtl) ++ [file: "index", bindings: [], app: __DIR__, folder: "priv/templates", ext: "html", bind_script: true]
 end
