3f318d7a17ca49925494421bad6f79581f0192c9
Merlin Gaillard <mirdhyn@gmail.com>
2014-02-19 15:02:03 +0100

    Fix Elixir build process change

--- lib/sample/n2o.ex
+++ lib/sample/n2o.ex
@@ -8,7 +8,7 @@ end
 defmodule N2O.Records do
     require N2O
     defrecord :context, Record.extract(:context, from_lib: "n2o/include/wf.hrl")
-    defrecord :http_req, Record.extract(:http_req, from_lib: "cowboy/src/cowboy_req.erl")
+    defrecord :http_req, Record.extract(:http_req, from: "deps/cowboy/src/cowboy_req.erl")
     defrecord :dtl, N2O.element(:element_dtl) ++ [file: "index", bindings: [], app: __DIR__, folder: "priv/templates", ext: "html", bind_script: true]
     defrecord :link, N2O.element(:element_link) ++ [target: :undefined, url: "javascript:void(0);", postback: :undefined, delegate: :undefined, name: :undefined]
     defrecord :button, N2O.element(:element_button) ++ [type: "button", name: :undefined, value: :undefined, postback: :undefined, delegate: :undefined, disabled: :undefined]
